934a43ecb366bcd0017463d9ad09bd1332815ff3
muramurasan <yasuhiro.matsumura@pixta.co.jp>
2015-10-05 16:20:58 +0900

    Revert "Merge branch 'static-pages'"
    This reverts commit cb5cc2f1f5d2d3477a48d10069f641de74e7a352, reversing
    changes made to b99b72f30bce28d4bb710a40872907fffd899c59.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
-<<<<<<< HEAD
 describe "Static pages" do
-=======
-describe "StaticPages" do
->>>>>>> static-pages
 
   describe "Home page" do
 
@@ -12,14 +8,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-<<<<<<< HEAD
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
-=======
->>>>>>> static-pages
   end
 
   describe "Help page" do
@@ -28,14 +21,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-<<<<<<< HEAD
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
-=======
->>>>>>> static-pages
   end
 
   describe "About page" do
@@ -44,16 +34,10 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-<<<<<<< HEAD
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
-=======
-  end
-
-
->>>>>>> static-pages
 end
