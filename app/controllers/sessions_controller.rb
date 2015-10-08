7687a92c3dd0207a881b8aab60eeab4ee72df7a2
muramurasan <yasuhiro.matsumura@pixta.co.jp>
2015-10-08 13:58:52 +0900

    Chapter8-2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーをサインインさせ、ユーザーページ(show)にリダイレクトする。
+      sign_in user
+      redirect_to user
     else
       # エラーメッセージを表示し、サインインフォームを再描画する。
       flash.now[:error] = 'Invalid email/password combination' #誤りあり!!
@@ -15,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    sign_out
+    redirect_to root_url
   end
 end
